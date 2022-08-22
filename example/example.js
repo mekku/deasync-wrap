317e18a28fcc442e0ae575cdbf4ea8fb2e9414c2
Nakara Kittisirikul <mekku.nk@gmail.com>
2022-08-22 11:55:22 +0700

    Update example.js

--- example/example.js
+++ example/example.js
@@ -14,9 +14,13 @@ async function crash(note, ms = 100) {
 try {
     const syncSleep = deasyncAsync(sleep);
 
+    // Async function must return right away with the word "Promise"
     console.log(syncSleep('sync'))
+
+    // Sync function must return when done  with the return of function
     console.log(sleep('async'))
 
+    // This line is an example when function is crash you can handle with try...cache
     const syncCrash = deasyncAsync(crash);
     console.log(syncCrash('sync'))
 }
